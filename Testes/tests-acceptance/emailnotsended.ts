5be435c0c9fa95386ef1315aa57533c8ca09bba6
godsavemenow <lvjs@cin.ufpe.br>
2019-06-15 06:30:30 -0300

    [FINISHES #39]finalização de implementação do teste de falha no envio de email

---
+++ Testes/tests-acceptance/emailnotsended.ts
@@ -0,0 +1,27 @@
+import { defineSupportCode } from 'cucumber';
+import { browser, $, element, ElementArrayFinder, by } from 'protractor';
+let chai = require('chai').use(require('chai-as-promised'));
+let expect = chai.expect;
+
+let sleep = (ms => new Promise(resolve => setTimeout(resolve, ms)));
+let sended = ((subject) => elem.element(by.name('subjectlist')).getText().then(text => text === subject));
+
+defineSupportCode(function ({ Given, When, Then }) {
+    Given(/^I am at the Email page$/, async () => {
+        await browser.get("http://localhost:4200/email");
+        await expect(browser.getTitle()).to.eventually.equal('Email');
+    })
+
+
+    When(/^I try to send the email with "([^\"]*)" as subject and "([^\"]*)" as body$/, async (subject, body) => {
+        await $("input[name='subjectbox']").sendKeys(<string> subject);
+        await $("input[name='bodybox']").sendKeys(<string> body);
+        await element(by.buttonText('Enviar')).click();
+    });
+
+
+    Then(/^I can't see the "([^\"]*)" in the sended list$/, async (subject) => {
+      var allsended : ElementArrayFinder = element.all(by.name('sendedlist'));
+        allsended.filter(elem => sended(subject)).then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
+    });
+})
