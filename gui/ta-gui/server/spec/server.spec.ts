946cc81bf158d12f6ae4ab922277f54a40254344
godsavemenow <lvjs@cin.ufpe.br>
2019-06-15 06:37:34 -0300

    [FINISHES #43]finalização da implementação do teste de serviço de buscar lista de emails

--- gui/ta-gui/server/spec/server.spec.ts
+++ gui/ta-gui/server/spec/server.spec.ts
@@ -18,4 +18,14 @@ describe("O servidor", () => {
      });
 
 
+  it("retorna a lista de destinatários do email", () => {
+   return request.post(base_url + "email", {"json":{"subject": "teste subject", "body" : "teste body"}}).then(body => {
+         expect(body).toEqual({success: "O email foi enviado com sucesso"});
+             return request.get(base_url + "email").then(body => {
+                 expect(body).toContain('{"subject": "teste subject", "body" : "teste body"}');
+             });
+         });
+     });
+  
+
   })
